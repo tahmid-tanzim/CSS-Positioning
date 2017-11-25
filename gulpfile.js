ba9ca7d8b77a51d3801f86f1e192f81cd3c6bd5e
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-11-25 19:27:21 +0600

    Browsersync integrated

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('app/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch('app/sass/**/*.scss', ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
